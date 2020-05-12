04ccde592243f9bf759ebc967daa46a3e7f889bc
Marin Ueda <marin96nym@gmail.com>
2020-05-12 22:46:59 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 植田真梨 on 2020/05/12.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
