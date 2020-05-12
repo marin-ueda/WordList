cc2779b5c055f3bd756d53cc17331ad8b65773cb
Marin Ueda <marin96nym@gmail.com>
2020-05-13 01:49:05 +0900

    2020/5/13

--- WordList/ListTableViewController.swift
+++ WordList/ListTableViewController.swift
@@ -9,22 +9,44 @@
 import UIKit
 
 class ListTableViewController: UIViewController {
-
+    
+    var wordArray: [Dictionary<String, String>] = []
+    
+    let saveData = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        if saveData.array(forKey: "WORD") != nil{
+            wordArray = saveData.array(forKey: "WORD") as! [Dictionary<String, String>]
+        }
+        
+       tableView.reloadData()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+}
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return wordArray.count
+}
+    
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ListTableViewCell
+        
+        let nowIndexPathDictionary = wordArray[indexPath.row]
+        
+        cell.englishLabel.text = nowIndexPathDictionary["english"]
+        cell.japaneseLabel.text = nowIndexPathDictionary["japanese"]
+        
+        return cell
+}
 }
